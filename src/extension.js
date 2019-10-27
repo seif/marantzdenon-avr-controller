9ae0159780d7e4bec4d851ce1be6627a6d06d3be
Sylvain Terrien <sylvainterrien@orange.fr>
2019-10-28 00:07:17 +0100

    Gnome 3.34 compatibility

--- src/extension.js
+++ src/extension.js
@@ -1,39 +1,34 @@
 /*
  * extension.js
  * Copyright (C) 2019 Sylvain Terrien <sylvainterrien@orange.fr>
- * 
+ *
  * Denon AVR controler is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Denon AVR controler is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-'use strict';
-
-const Clutter = imports.gi.Clutter;
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Soup = imports.gi.Soup;
 const Lang = imports.lang;
+const { Clutter, St, Soup, GObject, Gio } = imports.gi;
+const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Slider = imports.ui.slider;
-const Gio = imports.gi.Gio;
 
 // Other javascript files in the Denon_AVR_controler@sylter.fr directory are
 // accesible via Me.<file name>
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-const IndicatorName = "DenonAVRindicator";
+const IndicatorName = 'DenonAVRindicator';
 
 let denonAVRindicator;
 let httpSession = new Soup.SessionAsync();
@@ -41,108 +36,117 @@ Soup.Session.prototype.add_feature.call(httpSession, new Soup.ProxyResolverDefau
 
 let baseUrl;
 
-// borrowed from:
-// https://github.com/eonpatapon/gnome-shell-extensions-mediaplayer
-const SliderItem = new Lang.Class(
+var SliderItem = GObject.registerClass(
+class SliderItem extends PopupMenu.PopupImageMenuItem
 {
-    Name: 'SliderItem',
-    Extends: PopupMenu.PopupImageMenuItem,
-
-    _init: function(value, icon)
+    _init(value, icon, params)
     {
-        this.parent("", icon);
-        var layout = new Clutter.TableLayout();
+        super._init('', icon, params);
+        this.slider = new Slider.Slider(value);
+
+        let layout = new Clutter.TableLayout();
         this._box = new St.Widget({
-							style_class: 'slider-item',
-							layout_manager: layout});
+            style_class: 'slider-item',
+            layout_manager: layout
+        });
 
-        this._slider = new Slider.Slider(value);
-
-        layout.pack(this._slider.actor, 2, 0);
+        layout.pack(this.slider.actor, 2, 0);
         this.actor.add(this._box, {span: -1, expand: true});
-    },
+    }
 
-    setValue: function(value)
+    setValue(value)
     {
-        this._slider.setValue(value);
-    },
+        if (this.slider != undefined)
+        {
+            this.slider.value = value;
+        }
+    }
 
-    getValue: function()
+    getValue()
     {
-        return this._slider._getCurrentValue();
-    },
+        if (this.slider != undefined)
+        {
+            return this.slider._getCurrentValue();
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-    connect: function(signal, callback)
+    connect(signal, callback)
     {
-        this._slider.connect(signal, callback);
+        if (this.slider != undefined)
+        {
+            this.slider.connect(signal, callback);
+        }
     }
 });
 
-const VolumeSlider = new Lang.Class(
+var VolumeSlider = GObject.registerClass(
+class VolumeSlider extends SliderItem
 {
-    Name: 'VolumeSlider',
-    Extends: SliderItem,
-
-    _VOLUME_MIN: -80,
-    _VOLUME_MAX: 18,
-
-    _init: function(volume)
+    _init(volume, params)
     {
-        this.parent(0, ''); // value MUST be specified!
+        super._init(0, '', params); // value MUST be specified!
+
+        this._volume_min = -80;
+        this._volume_max = 18;
+
+        this.setIcon('audio-volume-medium-symbolic');
         this.setVolume(volume); // Set the real value.
-        this.setIcon("audio-volume-medium-symbolic");
-    },
+    }
 
-    setVolume: function(volume)
+    setVolume(volume)
     {
-        this.setValue((volume - this._VOLUME_MIN) / (this._VOLUME_MAX - this._VOLUME_MIN));
-    },
+        this.setValue((volume - this._volume_min) / (this._volume_max - this._volume_min));
+    }
 
-    getVolume: function()
+    getVolume()
     {
-        let value = this.getValue() * (this._VOLUME_MAX - this._VOLUME_MIN) + this._VOLUME_MIN;
+        let value = this.getValue() * (this._volume_max - this._volume_min) + this._volume_min;
         let volume = Math.floor(value);
         if (value - volume >= 0.5)
         {
             volume += 0.5;
         }
-        
+
         return volume;
-    },
+    }
 
     changeIcon()
     {
         let value = this.getValue();
-        
+
         if (value == 0)
         {
-            this.setIcon("audio-volume-muted-symbolic");
+            this.setIcon('audio-volume-muted-symbolic');
         }
         else if (value < 0.3)
         {
-            this.setIcon("audio-volume-low-symbolic");
+            this.setIcon('audio-volume-low-symbolic');
         }
         else if (value < 0.7)
         {
-            this.setIcon("audio-volume-medium-symbolic");
+            this.setIcon('audio-volume-medium-symbolic');
         }
         else
         {
-            this.setIcon("audio-volume-high-symbolic");
+            this.setIcon('audio-volume-high-symbolic');
         }
     }
 });
 
 /**
  * A simple label which only displays the given text.
- * 
+ *
  * @type {Lang.Class}
  */
 const LabelWidget = new Lang.Class({
-    Name: "LabelWidget",
+    Name: 'LabelWidget',
     Extends: PopupMenu.PopupBaseMenuItem,
 
-    _init: function(text){
+    _init: function(text) {
         this.parent({
             reactive: false // Can't be focused/clicked.
         });
@@ -156,11 +160,11 @@ const LabelWidget = new Lang.Class({
 
     /**
      * Set the text for this label.
-     * 
+     *
      * @param text
      *            the new text.
      */
-    setText: function(text){
+    setText: function(text) {
         this._label.text = text;
     }
 });
@@ -177,28 +181,28 @@ const DenonAVRindicator = new Lang.Class(
         this.icon = new St.Icon({icon_name: 'audio-speakers-symbolic', style_class: 'system-status-icon'});
         this.actor.add_child(this.icon);
 
-        this.powerButton = new PopupMenu.PopupSwitchMenuItem("AVR", false);
+        this.powerButton = new PopupMenu.PopupSwitchMenuItem('AVR', false);
         this.volumeSlider = new VolumeSlider(-33);
         this.volumeLabel = new LabelWidget(this.volumeSlider.getVolume().toString());
-        
+
         this.menu.addMenuItem(this.powerButton);
         this.menu.addMenuItem(this.volumeSlider);
         this.menu.addMenuItem(this.volumeLabel);
         this.menu.connect('open-state-changed', Lang.bind(this, this._updateStatus));
 
-        this.powerButton.connect("toggled", Lang.bind(this, this._togglePowerButton));
-        this.volumeSlider.connect('value-changed', Lang.bind(this, this._changeVolume));
+        this.powerButton.connect('toggled', Lang.bind(this, this._togglePowerButton));
+        this.volumeSlider.connect('notify::value', Lang.bind(this, this._changeVolume));
     },
 
     _togglePowerButton: function(item, state)
     {
         if (state)
         {
-            this._sendCommand("PutSystem_OnStandby", "ON");
+            this._sendCommand('PutSystem_OnStandby', 'ON');
         }
         else
         {
-            this._sendCommand("PutSystem_OnStandby", "STANDBY");
+            this._sendCommand('PutSystem_OnStandby', 'STANDBY');
         }
     },
 
@@ -206,8 +210,8 @@ const DenonAVRindicator = new Lang.Class(
     {
         let volume = this.volumeSlider.getVolume().toString();
 
-        this._sendCommand("PutMasterVolumeSet", volume);
-        
+        this._sendCommand('PutMasterVolumeSet', volume);
+
         this.volumeLabel.setText(volume);
         this.volumeSlider.changeIcon();
     },
@@ -215,7 +219,7 @@ const DenonAVRindicator = new Lang.Class(
     _sendCommand: function(command, arg)
     {
         let url = baseUrl + 'MainZone/index.put.asp?cmd0=' + command + '%2F' + arg;
-            
+
         // create an http message
         let request = Soup.Message.new('GET', url);
         // queue the http request
@@ -227,19 +231,19 @@ const DenonAVRindicator = new Lang.Class(
         if (open)
         {
             this.loadSettings();
-            
+
             let url = baseUrl + 'goform/formMainZone_MainZoneXml.xml';
             let request = Soup.Message.new('GET', url);
             httpSession.queue_message(request, Lang.bind(this, this._parseResponse));
         }
     },
 
-    _parseResponse: function (httpSession, message)
+    _parseResponse:  function(httpSession, message)
     {
         if (message.status_code == 200)
         {
             let data = message.response_body.data;
-            
+
             let regexName = new RegExp('<FriendlyName><value>(.+)<\/value><\/FriendlyName>', 'g');
             let regexPower = new RegExp('<ZonePower><value>([A-Z]+)<\/value><\/ZonePower>', 'g');
             let regexVolume = new RegExp('<MasterVolume><value>(\-?[0-9.]+)<\/value><\/MasterVolume>', 'g');
@@ -251,7 +255,7 @@ const DenonAVRindicator = new Lang.Class(
             this.powerButton.label.text = name;
             this.powerButton.setToggleState(state);
             this.volumeSlider.setVolume(volume);
-            this.volumeLabel.setText(this.volumeSlider.getVolume().toString())
+            this.volumeLabel.setText(this.volumeSlider.getVolume().toString());
         }
     },
 
@@ -266,15 +270,15 @@ const DenonAVRindicator = new Lang.Class(
                 Gio.SettingsSchemaSource.get_default(), false);
 
         this.settings = new Gio.Settings({
-            settings_schema : gschema.lookup('org.gnome.shell.extensions.denon-avr-controler', true)
+            settings_schema: gschema.lookup('org.gnome.shell.extensions.denon-avr-controler', true)
         });
-        
+
         baseUrl = this.settings.get_value('avr-url').unpack();
     }
 });
 
 function enable()
-{    
+{
     denonAVRindicator = new DenonAVRindicator();
     denonAVRindicator.loadSettings();
     Main.panel.addToStatusArea(IndicatorName, denonAVRindicator);
